d696d57e52e8c45a52d1458f6490a14a96b3d17b
y-takano@sofu.co.jp <takanoyu0319@icloud.com>
2023-04-03 09:43:50 +0900

    駒を交互に置いて色を変える所まで

---
+++ ④othello0.0.2/ImageMng.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace othello0._0._2
+{
+    public class ImageMng
+    {
+        /// <summary>
+        /// イメージハッシュ
+        /// Key:画像ファイル名
+        /// Value:Imageインスタンス
+        /// </summary>
+        static private Hashtable m_image_hash = new Hashtable();
+
+        /// <summary>
+        /// イメージのロード
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        static public Image loadImage(string filename)
+        {
+            //イメージがすでにロードされていればそれを返す
+            if (m_image_hash.ContainsKey(filename))
+                return (Image)m_image_hash[filename];
+
+            //イメージがロードされていなければ、ここでロードする
+            m_image_hash[filename] = Image.FromFile(filename);
+            return (Image)m_image_hash[filename];
+        }
+        /// <summary>
+        /// イメージ削除
+        /// </summary>
+        /// <param name="filename"></param>
+        static public void delImage(string filename)
+        {
+            m_image_hash.Remove(filename);
+        }
+        /// <summary>
+        /// イメージ全削除
+        /// </summary>
+        static public void delAllImage()
+        {
+            m_image_hash.Clear();
+        }
+    }
+}
---
+++ ④othello0.0.2/Sprite.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace othello0._0._2
+{
+    public class Sprite
+    {
+        //スプライトリスト
+        List<Sprite> m_sprite_list = new List<Sprite>();
+        //表示位置
+        private Point m_pos = new Point();
+        //透明度
+        private float m_opacity = 0.0f;
+
+        /// <summary>
+        /// 描画
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="im"></param>
+        public void DrawImage(Graphics g, Image im)
+        {
+            DrawImage(g, im, Opacity);
+        }
+
+        /// <summary>
+        /// 描画
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="im"></param>
+        public void DrawImage(Graphics g, Image im, float opacity)
+        {
+            ColorMatrix cm = new ColorMatrix();
+            cm.Matrix00 = cm.Matrix11 = cm.Matrix22 = 1;
+            cm.Matrix33 = opacity;
+            cm.Matrix44 = 1;
+            ImageAttributes ia = new ImageAttributes();
+            ia.SetColorMatrix(cm);
+            Rectangle rc = new Rectangle();
+            rc.X = Pos.X;
+            rc.Y = Pos.Y;
+            rc.Width = im.Size.Width;
+            rc.Height = im.Size.Height;
+
+            g.DrawImage(im, rc, 0, 0, im.Width, im.Height, GraphicsUnit.Pixel, ia);
+
+        }
+
+        /// <summary>
+        /// 表示位置プロパティ
+        /// </summary>
+        public Point Pos
+        {
+            get { return m_pos; }
+            set { m_pos = value; }
+        }
+
+        /// <summary>
+        /// 透明度プロパティ
+        /// </summary>
+        public float Opacity
+        {
+            get { return m_opacity; }
+            set { m_opacity = value; }
+        }
+
+        /// <summary>
+        /// スプライト追加
+        /// </summary>
+        /// <param name=""></param>
+        public void addSprite(Sprite　sp)
+        {
+            m_sprite_list.Add(sp);
+        }
+        /// <summary>
+        /// スプライト削除
+        /// </summary>
+        public void delSprite(Sprite sp)
+        {
+            m_sprite_list.Remove(sp);
+        }
+
+        /// <summary>
+        /// スプライト全削除
+        /// </summary>
+        public void delAllSprite()
+        {
+            m_sprite_list.Clear();
+        }
+
+        //変数処理  
+        public virtual void onMove()
+        {
+            foreach (var sp in m_sprite_list)
+                sp.onMove();
+        }
+
+        //描画処理
+        public virtual void onDraw(Graphics g)
+        {
+            foreach (var sp in m_sprite_list)
+                sp.onDraw(g);
+        }
+
+        //マウスクリック処理
+        public virtual bool clickMouse(Point pos)
+        {
+            int cnt = m_sprite_list.Count;
+            for (int i = cnt - 1; i >= 0; --i)
+                if (m_sprite_list[i].clickMouse(pos))
+                    return true;
+            return false;
+        }
+
+        /// <summary>
+        /// マウス位置処理
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        public virtual bool moveMouse(Point pos)
+        {
+            int cnt = m_sprite_list.Count;
+            for (int i = cnt - 1; i >= 0; --i)
+                if (m_sprite_list[i].moveMouse(pos))
+                    return true;
+            return false;
+        }
+    }
+}
