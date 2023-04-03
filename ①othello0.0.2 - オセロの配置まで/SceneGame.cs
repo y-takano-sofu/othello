245a652d52ea6944350ecfc40948786525c1afec
y-takano@sofu.co.jp <takanoyu0319@icloud.com>
2023-04-03 09:09:06 +0900

    オセロの配置まで作成しました”

---
+++ ①othello0.0.2 - オセロの配置まで/SceneGame.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace othello0._0._2
+{
+    class SceneGame :Sprite
+    {
+        //背景
+        private SpriteBg m_bg = null;
+        //駒
+        private SpriteKoma[] m_koma = new SpriteKoma[64];
+        //手ばん
+        private SpriteKoma.KomaType m_teban = SpriteKoma.KomaType.Black;
+
+
+        public SceneGame() : base()
+        {
+            m_bg = new SpriteBg();
+            for (int i = 0; i < m_koma.Length; ++i)
+                m_koma[i] = new SpriteKoma();
+            
+            addSprite(m_bg);
+            foreach (var sp in m_koma)
+                addSprite(sp);
+
+            for (int row = 0; row < 8; ++row)
+            {
+                for (int line = 0; line < 8; ++line)
+                {
+                    var sp = getSpriteKoma(row, line);
+                    sp.Pos = new Point(row * 90, line * 90);
+                }
+            }
+        }
+
+        public void clearGame()
+        {
+            foreach (var sp in m_koma)
+                sp.Opacity = 0.0f;
+
+            m_bg.Opacity = 1.0f;
+
+
+            getSpriteKoma(3, 3).Type = SpriteKoma.KomaType.Black;
+            getSpriteKoma(4, 3).Type = SpriteKoma.KomaType.White;
+            getSpriteKoma(3, 4).Type = SpriteKoma.KomaType.White;
+            getSpriteKoma(4, 4).Type = SpriteKoma.KomaType.Black;
+
+            getSpriteKoma(3, 3).Opacity = 1.0f;
+            getSpriteKoma(4, 3).Opacity = 1.0f;
+            getSpriteKoma(3, 4).Opacity = 1.0f;
+            getSpriteKoma(4, 4).Opacity = 1.0f;
+
+            m_teban = SpriteKoma.KomaType.Black;
+
+        }
+
+        /// <summary>
+        /// 駒スプライト取得
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public SpriteKoma getSpriteKoma(int row, int line)
+        {
+            if (line < 0 || line >= 8 || row < 0 || row >= 8)
+                return null;
+            return m_koma[line * 8 + row];
+        }
+
+
+
+        public override void onMove()
+        {
+            base.onMove();
+        }
+
+        public override void onDraw(Graphics g)
+        {
+            base.onDraw(g);
+        }
+
+        public override bool clickMouse(Point pos)
+        {
+            if (base.clickMouse(pos))
+                return true;
+            return false;
+        }
+
+        public override bool moveMouse(Point pos)
+        {
+            if (base.moveMouse(pos))
+                return true;
+            return false;
+        }
+
+
+    }
+}
---
+++ ①othello0.0.2 - オセロの配置まで/SpriteKoma.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace othello0._0._2
+{
+    /// <summary>
+    /// 駒クラス
+    /// </summary>
+    class SpriteKoma : Sprite
+    {
+
+        /// <summary>
+        /// 駒タイプ
+        /// </summary>
+        public enum KomaType
+        {
+            None　= 0,　//なし
+            Black = 1,  //黒
+            White = 2,  //白
+        }
+
+        private KomaType m_koma_type = KomaType.None;
+
+
+        //黒
+        Image m_black = null;
+        //白
+        Image m_white = null;
+
+
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public SpriteKoma() : base()
+        {
+            m_black = ImageMng.loadImage("img/black.png");
+            m_white = ImageMng.loadImage("img/white.png");
+
+        }
+
+        public KomaType Type
+        {
+            get { return m_koma_type;}
+            set { m_koma_type = value;}
+        }
+
+        public override void onMove()
+        {
+            base.onMove();
+        }
+        public override void onDraw(Graphics g)
+        {
+            base.onDraw(g);
+            if (Type == KomaType.None)
+                return;
+            if (Type == KomaType.Black)
+                DrawImage(g, m_black);
+            if (Type == KomaType.White)
+                DrawImage(g, m_white);
+
+        }
+
+        public override bool clickMouse(Point pos)
+        {
+            if (base.clickMouse(pos))
+                return true;
+            return false;
+        }
+        public override bool moveMouse(Point pos)
+        {
+            if (base.moveMouse(pos))
+                return true;
+            return false;
+        }
+    }
+}
